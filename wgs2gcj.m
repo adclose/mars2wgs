6b558e1cdb9ea5fdd3111f09dabda583cbe02a27
Aaron Close <adclose@gmail.com>
2017-11-28 15:19:27 -0600

    Initial set of files for project
    mars2wgs is my own algorithm and from my tests performs much better than the gcj2wgs version which is based on https://github.com/googollee/eviltransform method
    This version often arrives at a solution in only 3 iterations and seems very stable.  I would appreciate any feedback from users

---
+++ wgs2gcj.m
@@ -0,0 +1,44 @@
+function [lat,long] = wgs2gcj(lat,long);
+
+a = 6378245.0;
+ee = 0.00669342162296594323;
+if(outOfChina(lat,long)),return; end;
+
+ dLat = transformLat(lat,long);
+ dLon = transformLong(lat,long);
+ radLat = lat / 180.0 * pi;
+ magic = sin(radLat);
+ magic = 1 - ee * magic.^2;
+ sqrtMagic = magic.^(.5);
+ dLat = (dLat * 180.0)./((a * (1 - ee))./(magic.*sqrtMagic)*pi);
+ dLon = (dLon * 180.0)./(a./sqrtMagic.*cos(radLat)*pi);
+ lat = lat + dLat;
+ long = long + dLon;
+
+end
+
+function result = outOfChina(lat,long)
+    result = false;
+    if any(long < 72.004 | long > 137.8347) result = true; end
+    if any(lat < 0.8293 | lat > 55.8271) result = true; end
+end
+
+function deltaLong = transformLong(lat,long)
+    x = long -105;
+    y = lat -35;
+    deltaLong = -100+ 2*x + 3*y + 0.2*y.^2 + 0.1.*x.*y + 0.2*sqrt(abs(x))+...
+    (20*sin(6.*x.*pi)+20*sin(2.*x.*pi))*2/3 +...
+    (20*sin(y.*pi) + 40*sin(y./3.*pi))*2/3 +...
+    (160*sin(y./12.*pi) + 320*sin(y.*pi./30))*2/3;
+end
+
+
+function deltaLat = transformLat(lat,long)
+    x = long -105;
+    y = lat -35;
+    deltaLat = 300 + x + 2*y + 0.1*x.^2 + 0.1*x.*y + 0.1.*sqrt(abs(x))+...
+    2/3*((20*sin(6*pi*x) + 20*sin(2*pi*x))+ ...
+    (20*sin(pi*x) + 40*sin(pi/3*x))+ ...
+    (150*sin(pi/12*x) + 300*sin(pi/30*x)));
+
+end
